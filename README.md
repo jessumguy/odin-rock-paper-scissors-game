2a82eda8d7b6bf9fb7840f917e4a4aa29b7b4ef0
jessumguy <yongjohnathonch@gmail.com>
2022-06-06 11:42:57 +0800

    chore: 1.0 release - Add page styling and general refactoring of code.

--- README.md
+++ README.md
@@ -2,7 +2,47 @@
 
 The Odin Project - Rock Paper Scissors Project
 
-**26/02/2022** - A simple rock paper scissors game played through the console. 
+Site: https://jessumguy.github.io/rock-paper-scissors-game/
+
+![](https://github.com/jessumguy/rock-paper-scissors-game/blob/main/odin_project_rps_with_UI.gif)
+
+**06/06/2022** - Graphical UI added.
+
+Graphical UI and general code refactoring added back around late April / early May 2022, but held off from pushing at the time as I wanted to make the UI better. However, after some thought, I've come to the realization that I was spending way too much time on trying to perfect the samller details of this project. Time that would be better spent on moving on with the course. As such, this will be considered the 1.0 release.
+
+**Reflection:** From the top of my head, in no particular order:
+
+1. No simple way to 'pause' a loop so as to input new values.
+
+The console-only version of this game used a for loop in the playGame() function to loop through each round to decide the winner/loser. Each loop would pause at the user prompt() method in the playerSelection() function whereby the user would then input a new choice (rock, paper or scissors) for the next round.
+
+Upon adding choice buttons for the user input, this prompt() method became redundant and was removed. I was unable to find a simple way to 'pause' between each loop for new user choices. Clicking any button would 'lock' this value as playerSelection for the entire loop of five rounds. 
+
+Ultimately, the simplest solution was to remove the for loop entirely, and to add a condition to disable the choice button after 5 rounds.
+
+2. Global Variables 
+
+Don't quite remember the details, but the gameRound, playerScore and computerScore variables originally in the playGame() function did not tally up correctly after removing the for loop. Being unable to come up with a solution, I ended up make these global variables.
+
+3. Comments in code
+
+Comments are useful to remind myself on what a code block is expected to do after some time off.
+
+4. Page Styling 
+
+When opeining a page, the background and CSS takes a split second to load up. The unstyled version of page in this time is very noticable and unappealing. Still discovering how to make a page load in the styling before appearing on the page.
+
+**Other Notes/Credits:**
+
+- Backgound from <a href="https://wallpapercave.com/w/wp1933957">wallpapercave</a>
+- Button design from <a href="https://github.com/wesbos/JavaScript30/tree/master/01%20-%20JavaScript%20Drum%20Kit">Wesbos's JavaScript 30 - 01 JavaScript Drum Kit</a></li>
+- Page / UI styling is not easy - It is difficult to come up with something that is aestetically pleasing in both look and feel.
+- Adding fields to the DOM using javascript can lead to a pretty messy file.
+
+---
+
+
+**26/02/2022** A simple rock paper scissors game played through the console. 
 
 Site: https://jessumguy.github.io/rock-paper-scissors-game/
 
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let gameRound = 0;
+let playerScore = 0;
+let computerScore = 0;
+
 // CPU selection of Rock, Paper or Scissors.
 let computerPlay = () => {
   let computerSelection = returnRandomChoice().toString();
@@ -8,7 +12,6 @@ let computerPlay = () => {
 // Function that returns randomized values: Rock, Paper or Scissors. 
 let returnRandomChoice = () => {
   let choice = Math.floor(Math.random() * 3);
-  //console.log(choice);
 
   switch (choice) {
     case 0:
@@ -27,22 +30,12 @@ let returnRandomChoice = () => {
   return choice;
 }
 
-// console.log(computerPlay());
-
-// Player's input of value into prompt: Rock, Paper or Scissors. Input is case-insensitive.
-let playerPlay = () => {
-  let playerSelection = prompt('Rock, paper or scissors')?.toLowerCase();
-
-  if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors'){
-    return playerSelection;
-  } else {
-    playerSelection = false;
-    console.log('Invalid Input'); 
-  }
-} 
 
 // Function that compares the playerSelection with the computerSelection.
 let playRound = (playerSelection, computerSelection) => {
+
+  pChoice.textContent = `Player Choice: ${playerSelection}`;
+  cChoice.textContent = `Computer Choice: ${computerSelection}`;
 
   console.log(`Player Choice: ${playerSelection} \n Computer Choice: ${computerSelection}`);
 
@@ -76,55 +69,104 @@ let playRound = (playerSelection, computerSelection) => {
   } 
 }
 
-// playRound('rock', 'paper');
-// playRound('paper', 'paper');
 
 // function that plays five games, keeping track of each player's score, and passes it to the announceGameResult().
 let playGame = () => {
+  let playerSelection = playerChoice;
+  let computerSelection = computerPlay();
 
-  let gameRound = 0;
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = playerPlay();
-    let computerSelection = computerPlay();
-    gameRound++
-
-    switch(playRound(playerSelection, computerSelection)) {
-      case("WIN"):
-        console.log(`Round ${i+1}: WON`);
-        playerScore++;
-        break;
-      case("LOSE"):
-        console.log(`Round ${i+1}: LOSS`);
-        computerScore++;
-        break;
-      default:
-        console.log(`Round ${i+1}: TIE`);
-    }
-
-    console.log(`Player Score ${playerScore}`);
-    console.log(`Computer Score ${computerScore}`);
+  switch(playRound(playerSelection, computerSelection)) {
+    case("WIN"):
+      gRound.textContent = `Round ${gameRound+1}: WON`;
+      console.log(`Round ${gameRound+1}: WON`);
+      playerScore++;
+      break;
+    case("LOSE"):
+      gRound.textContent = `Round ${gameRound+1}: LOSS`;
+      console.log(`Round ${gameRound+1}: LOSS`);
+      computerScore++;
+      break;
+    default:
+      gRound.textContent = `Round ${gameRound+1}: TIE`;
+      console.log(`Round ${gameRound+1}: TIE`);
   }
+  gameRound++
 
-  announceGameResult(playerScore, computerScore);
+  playerScoring.textContent = playerScore;
+  computerScoring.textContent = computerScore;
+  console.log(`Player Score ${playerScore}`);
+  console.log(`Computer Score ${computerScore}`);
+  
+  if (gameRound === 5) {
+    announceGameResult(playerScore, computerScore);
+  }
 }
 
 
 // Function that takes score from the playGame(), and evaluates game result from the player's perspective
 let announceGameResult = (playerScore, computerScore) => {
+  
   if (playerScore > computerScore) {
+    gResult.textContent = `Game Won!`
     console.log("Game Won!")
   } else if (playerScore < computerScore){
+    gResult.textContent = `Game Loss!`
     console.log("Game Loss!")
   } else {
+    gResult.textContent = `Game Tie!`
     console.log("Game Tie!")
   }
+
+  // Disables buttons after 5 rounds and add resetButton to DOM
+  for (let i = 0; i < rpsButtons.length; i++) {
+    rpsButtons[i].disabled = true;
+    rpsButtons[i].classList.add('disabled');
+    rpsButtons[i].classList.remove('btn_hover')
+  }
+
+  roundResults.appendChild(resetButton);
+  resetButton.classList.add('btn_hover')
 }
 
-// console.log(announceGameResult(3,2)); // Game Won
-// console.log(announceGameResult(2,2)); // Game Tie
-// console.log(announceGameResult(2,3)); // Game Loss
+// rpsButtons to change state of buttons after an event - Used in nnounceGameResult()
+const rpsButtons = document.getElementById('player-selections').children;
 
-playGame();
+
+// Assign value to buttons
+const playerSelections = document.getElementById('player-selections').querySelectorAll('button');
+
+playerSelections.forEach(button => button.addEventListener('click', (e) => {
+  playerChoice = e.target.value;
+  playGame();
+}));
+
+
+// Player and CPU Scoreboard - Value is dynamically increments in announceGameResult()
+const playerScoreboard = document.querySelector('#playerscore');
+const playerScoring = document.createElement('p');
+playerScoreboard.appendChild(playerScoring);
+
+const computerScoreboard = document.querySelector('#computerscore');
+const computerScoring = document.createElement('p');
+computerScoreboard.appendChild(computerScoring);
+
+
+// Status Report - Displays the score and result of each round
+const roundResults = document.querySelector('#round-results');
+const gRound = document.createElement('p');
+const pChoice = document.createElement('p');
+const cChoice = document.createElement('p');
+const gResult = document.createElement('p');
+roundResults.appendChild(gRound);
+roundResults.appendChild(pChoice);
+roundResults.appendChild(cChoice);
+roundResults.appendChild(gResult);
+
+// Reset button refreshes the page. It is added to DOM after 5 rounds in announceGameResult()
+const resetButton = document.createElement('button');
+resetButton.textContent = `RESET`;
+
+resetButton.addEventListener('click', (refresh) => {
+  location.reload();
+});
+
